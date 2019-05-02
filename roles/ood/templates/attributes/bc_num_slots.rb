84c506c8f93886bf8b88cfe090c90f492f33bc62
Bo-Chun Louis Chen <louistw@uab.edu>
2019-05-02 11:16:08 -0500

    Update bc_num_slots in global attribute

---
+++ roles/ood/templates/attributes/bc_num_slots.rb
@@ -0,0 +1,53 @@
+module SmartAttributes
+  class AttributeFactory
+    # Build this attribute object with defined options
+    # @param opts [Hash] attribute's options
+    # @return [Attributes::BCNumSlots] the attribute object
+    def self.build_bc_num_slots(opts = {})
+      Attributes::BCNumSlots.new("bc_num_slots", opts)
+    end
+  end
+
+  module Attributes
+    class BCNumSlots < Attribute
+      # Hash of options used to define this attribute
+      # @return [Hash] attribute options
+      def opts
+        @opts.reverse_merge(min: 1, step: 1)
+      end
+
+      # Value of attribute
+      # @return [String] attribute value
+      def value
+        (opts[:value] || "1").to_s
+      end
+
+      # Type of form widget used for this attribute
+      # @return [String] widget type
+      def widget
+        "number_field"
+      end
+
+      # Form label for this attribute
+      # @param fmt [String, nil] formatting of form label
+      # @return [String] form label
+      def label(fmt: nil)
+        str = opts[:label] || "Number of CPU"
+      end
+
+      # Whether this attribute is required
+      # @return [Boolean] is required
+      def required
+        true
+      end
+
+      # Submission hash describing how to submit this attribute
+      # @param fmt [String, nil] formatting of hash
+      # @return [Hash] submission hash
+      def submit(fmt: nil)
+        slots = value.blank? ? 1 : value.to_i
+        { script: { native: ["-n", slots] } }
+      end
+    end
+  end
+end
