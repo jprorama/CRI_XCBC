160f89dfcb938bb9c88ecad3259a72cc61ddd355
Bo-Chun Louis Chen <louistw@uab.edu>
2024-08-23 10:21:48 -0500

    refactor: Extract config into config file

--- roles/ood_proxy/files/rewrite_map.py
+++ roles/ood_proxy/files/rewrite_map.py
@@ -1,19 +1,16 @@
 #!/usr/bin/env python3
 import grp
 import sys
-
-target_grp = 'gpfs5'
-target_hostname = 'login002.cm.cluster'
-DEBUG = False
+import rewritemap_config as cfg
 
 while sys.stdin:
     try:
         username = sys.stdin.readline().strip()   ## It is very important to use strip!
-        if DEBUG: print(username)
+        if cfg.DEBUG: print(username)
         if not username:
             print('NULL')
-        if username in grp.getgrnam(target_grp).gr_mem:
-            print(target_hostname)
+        if username in grp.getgrnam(cfg.target_grp).gr_mem:
+            print(cfg.target_hostname)
         else:
             print('NULL')
         sys.stdout.flush()
---
+++ roles/ood_proxy/files/rewritemap_config.py
@@ -0,0 +1,4 @@
+DEBUG = False
+target_grp = 'gpfs5'
+target_hostname = 'login002.cm.cluster'
+
